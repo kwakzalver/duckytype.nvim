a42627579896cee2df0010abf2bd3d5ea724a4c1
bryant-the-coder <bryantfoo99@gmail.com>
2022-04-24 17:21:23 +0800

    feat(ui): Added borders

--- lua/duckytype/settings.lua
+++ lua/duckytype/settings.lua
@@ -1,29 +1,62 @@
+-- local border = {
+--     { "┏", "FloatBorder" },
+--     { "━", "FloatBorder" },
+--     { "┓", "FloatBorder" },
+--     { "┃", "FloatBorder" },
+--     { "┛", "FloatBorder" },
+--     { "━", "FloatBorder" },
+--     { "┗", "FloatBorder" },
+--     { "┃", "FloatBorder" },
+-- }
+
+-- local border = {
+-- 	{ "╔", "FloatBorder" },
+-- 	{ "═", "FloatBorder" },
+-- 	{ "╗", "FloatBorder" },
+-- 	{ "║", "FloatBorder" },
+-- 	{ "╝", "FloatBorder" },
+-- 	{ "═", "FloatBorder" },
+-- 	{ "╚", "FloatBorder" },
+-- 	{ "║", "FloatBorder" },
+-- }
+
+local border = {
+	{ "╭", "CmpBorder" },
+	{ "─", "CmpBorder" },
+	{ "╮", "CmpBorder" },
+	{ "│", "CmpBorder" },
+	{ "╯", "CmpBorder" },
+	{ "─", "CmpBorder" },
+	{ "╰", "CmpBorder" },
+	{ "│", "CmpBorder" },
+}
+
 local Defaults = {
-  expected = "english_common",
-  number_of_words = 50,
-  -- assuming an average word is 5.8 characters long - guesstimate which is
-  -- based on the average length of the english top 1000 common words,
-  -- MonkeyType uses 5, so if you want higher scores to stroke your ego...
-  average_word_length = 5.8,
+	expected = "english_common",
+	number_of_words = 50,
+	-- assuming an average word is 5.8 characters long - guesstimate which is
+	-- based on the average length of the english top 1000 common words,
+	-- MonkeyType uses 5, so if you want higher scores to stroke your ego...
+	average_word_length = 5.8,
 
-  -- maybe the little pop-up window looks better when centered instead
-  window_config = {
-    style = 'minimal',
-    border = 'single',
-    relative = 'win',
-    focusable = true,
-    col = 1,
-    row = 1,
-    width = 80,
-    height = 8,
-  },
+	-- maybe the little pop-up window looks better when centered instead
+	window_config = {
+		style = "minimal",
+		border = border,
+		relative = "win",
+		focusable = true,
+		col = 1,
+		row = 1,
+		width = 80,
+		height = 8,
+	},
 
-  -- link correct/incorrect/remaining to highlighting groups
-  highlight = {
-    good = "Todo",
-    bad = "Error",
-    remaining = "Function",
-  },
+	-- link correct/incorrect/remaining to highlighting groups
+	highlight = {
+		good = "Todo",
+		bad = "Error",
+		remaining = "Function",
+	},
 }
 
 return Defaults
