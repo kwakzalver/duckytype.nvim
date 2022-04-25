729f150894ffe985f49b9e83403011fbf865be13
duck <kwakzalver@github.com>
2022-04-25 13:26:27 +0200

    Unexpose RedrawBuffer, it was a cheap trick

--- lua/duckytype/init.lua
+++ lua/duckytype/init.lua
@@ -2,7 +2,6 @@ local methods = require('duckytype.methods')
 
 local Plugin = {
   setup = methods.Setup,
-  RedrawBuffer = methods.RedrawBuffer,
   Start = methods.Start,
   NewGame = methods.NewGame,
 }
